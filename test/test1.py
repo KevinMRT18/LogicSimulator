8b2dc95ad30d5c435898837925dabac874e72deb
joshu <joshuanenadich98@gmail.com>
2021-04-07 20:06:37 -0400

    First Commit

--- test/test1.py
+++ test/test1.py
@@ -3,11 +3,13 @@ from src.components import *
 import pytest
 
 a = AndGate('and1')
-
+b = XorGate('xor1')
 
 
 
 def test_out():
     a.output([1, 1])
     assert a.out == 1, 'The result expected was 1'
+    b.output([1, 1])
+    assert b.out == 0, 'The result expected was 0'
 
